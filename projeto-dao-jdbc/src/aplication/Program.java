00599c4e47c34773e0d0d985843ff84cd31a44e3
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-07-09 14:50:36 -0300

    Criação de Classe
    Criação da Classe Seller

--- projeto-dao-jdbc/src/aplication/Program.java
+++ projeto-dao-jdbc/src/aplication/Program.java
@@ -1,12 +1,19 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		
 		Department obj = new Department(1, "TVs");
-		System.out.println(obj);
+		Seller sel = new Seller(1, "CAsa", "sds@gmail", new Date(), 300.00, obj);
+		
+		System.out.println(sel);
 
 	}
 
