3bfa253f1bb120168f127f4f7b86f7f0c11a5f4b
DamiaoLustosa <50031115+DamiaoLustosa@users.noreply.github.com>
2019-07-09 22:43:20 -0300

    Usando map para associação-jdbc
    Implementando Map na classe SellerDaoJDBC para fazer a associação correta entre sellers e departments

--- projeto-dao-jdbc/src/aplication/Program.java
+++ projeto-dao-jdbc/src/aplication/Program.java
@@ -1,18 +1,30 @@
 package aplication;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
+		System.out.println("==== TESTE 1: seller findByID ==== ");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
 		Seller sel = sellerDao.findById(3);
-		
+
 		System.out.println(sel);
+
+		System.out.println("\n ==== TESTE 2: seller findByDepartment ==== ");
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+
+		}
 
 	}
 
